ba67fcf9fd466d668aee944d5bf29e82ad1665b6
hadams0 <adamshannah1@icloud.com>
2023-07-25 12:30:25 +0100

    changed flights to flightList

--- src/main/java/Airline.java
+++ src/main/java/Airline.java
@@ -3,17 +3,21 @@ import java.util.ArrayList;
 
 public class Airline {
 
-    private ArrayList<Flight> flights;
+    private ArrayList<Flight> flightList;
 
-    public Airline(ArrayList<Flight> flights) {
-        this.flights = flights;
+    public Airline(ArrayList<Flight> flightList) {
+        this.flightList = flightList;
     }
 
     public ArrayList<Flight> getFlights() {
-        return flights;
+        return flightList;
     }
 
-    public void setFlights(ArrayList<Flight> flights) {
-        this.flights = flights;
+    public void setFlights(ArrayList<Flight> flightList) {
+        this.flightList = flightList;
+    }
+
+    public void addFlight (Flight flight){
+        flightList.add(flight);
     }
 }
