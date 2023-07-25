0507ddee8f575f926bee0f7b291d2bc8713ba2a7
amelieboydshire <aboydshire10@gmail.com>
2023-07-25 12:08:56 +0100

    passenger getters and setters

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -9,4 +9,28 @@ public class Passenger {
         this.contactNumber = contactNumber;
         this.passengerId = passengerId;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public int getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(int passengerId) {
+        this.passengerId = passengerId;
+    }
 }
