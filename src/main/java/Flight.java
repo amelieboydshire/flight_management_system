32ca23fc3b3547f0ba12b41827500aa8a3c846e9
amelieboydshire <aboydshire10@gmail.com>
2023-07-25 12:13:19 +0100

    flight constructor

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -4,4 +4,12 @@ public class Flight {
     private String destination;
     private int flightId;
     private ArrayList<Passenger> passengers;
+
+    public Flight(String destination, int flightId, ArrayList<Passenger> passengers) {
+        this.destination = destination;
+        this.flightId = flightId;
+        this.passengers = passengers;
+    }
+
+   
 }
