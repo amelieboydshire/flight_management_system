1c40bbb0ed1fb9270e979091119bec57b2df011a
hadams0 <adamshannah1@icloud.com>
2023-07-25 15:10:28 +0100

    fixed errors

--- src/test/java/AirlineTest.java
+++ src/test/java/AirlineTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class AirlineTest {
+
+    private ArrayList<Flight> flightList;
+
+    @BeforeEach
+    public void setUp(){
+        Flight flight1 = new Flight("Spain", 1);
+        Flight flight2 = new Flight("Portugal", 2);
+        flightList.add(flight1);
+        flightList.add(flight2);
+    }
+
+    @Test
+    public void canGetFlights(){
+
+    }
 }
