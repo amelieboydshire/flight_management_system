89a031f1735c63c43f3c65ed7414190f10388e13
amelieboydshire <aboydshire10@gmail.com>
2023-07-25 12:05:01 +0100

    passenger constructor

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,12 @@
 public class Passenger {
+
+    private String name;
+    private int contactNumber;
+    private int passengerId;
+
+    public Passenger(String name, int contactNumber, int passengerId) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.passengerId = passengerId;
+    }
 }
